fc7410f68625e5e951a63226a751bb158d942fbf
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-07-09 11:42:27 +0530

    sdm660-common: Move NFC check script to vendor
    * Fixes running on roms without backup tool
    * Self destruct after running
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -268,6 +268,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    device_check.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
